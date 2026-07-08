b7d0aa17b992c7e22a4eb0f72693f1b7aafbd9ab
Bora305 <97860744+Bora305@users.noreply.github.com>
2026-07-08 23:37:04 +0900

    config.py

--- config.py
+++ config.py
@@ -3,11 +3,24 @@ import os
 DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK', '')
 
 KEYWORDS = [
-    "RTX",
-    "노트북",
-    "CPU",
-    "그래픽카드",
-    "GPU",
+    "13900",
+    "13900K",
+    "13900KF",
+    "13900F",
+    "14900",
+    "14900F",
+    "14900KF",
+    "14900K",
+    "13700",
+    "13700F",
+    "13700KF",
+    "13700K",
+    "14700",
+    "14700F",
+    "14700KF",
+    "14700K",
+    "265K",
+    "285K",
 ]
 
 CHECK_INTERVAL = 5
